26902f99b5cbd63c7b07537307c8fc8fc167b763
Edward Frempong <asamoah.frempong@gmail.com>
2021-03-27 04:49:04 +0000

    reactclass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component,  useState} from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends Component {
+   constructor(){
+   super();
+   this.state= {age:60};
+ }
+
+ handleClick =()=>{ 
+   let newAge = this.state.age + 1;
+   this.setState({age:newAge})
+
+ }
+ render(){
+   return(
+   <div>
+     <h1>Age is {this.state.age}</h1>
+          <button onClick={this.handleClick}>Click Me!!!</button>
+   </div>
+   )
+ };
+  
+ }
 
 export default App;
